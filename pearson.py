839e4282538064cfe5f4f8199cbb6adb06644456
Alex <a.sandrovschii@gmail.com>
2022-10-05 21:02:36 +0200

    calculating the pearson_r values

--- pearson.py
+++ pearson.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy
-top_fertilizers = pd.read_csv('./top_fertilizers.csv')
+statistical_data_df = pd.read_csv('./all_combined.csv')
+statistical_data_df_cleaned = statistical_data_df.dropna()
 print()
-results = scipy.stats.pearsonr(top_fertilizers['Total fertilizer use per area of cropland in kg per hectare'],top_fertilizers['Number of deaths from digestive diseases'])
-print(results)
+results1 = scipy.stats.pearsonr(statistical_data_df_cleaned['Total fertilizer use per area of cropland in kg per hectare'],statistical_data_df_cleaned['Number of deaths from digestive diseases'])
+print("Fertilizers and death", results1)
+results2 = scipy.stats.pearsonr(statistical_data_df_cleaned['Pesticides used per area of cropland in kg per hectare'],statistical_data_df_cleaned['Number of deaths from digestive diseases'])
+print("Pesticides and death", results2)
+results3 = scipy.stats.pearsonr(statistical_data_df_cleaned['Total fertilizer use per area of cropland in kg per hectare'],statistical_data_df_cleaned['Total_food_supply_quantity, kg'])
+print("Fertilizers and quantity", results3)
+results4 = scipy.stats.pearsonr(statistical_data_df_cleaned['Total fertilizer use per area of cropland in kg per hectare'],statistical_data_df_cleaned['Total_food_supply_quality, kcal/capita/day'])
+print("Fertilizers and quality", results4)
+
+correlation_df = statistical_data_df_cleaned['Total fertilizer use per area of cropland in kg per hectare'].corr(statistical_data_df_cleaned['Number of deaths from digestive diseases']);
+print("Correlation: ", correlation_df)
